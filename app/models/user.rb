05b99da31c3b2b56a64c6f46d384133f6f2b6054
Peter Mumford <peter@petermumford.com>
2016-03-18 22:05:26 +0000

    Move email sending to the model and use Sidekiq
    To improve the slowness when creating a user I’ve moved the sending of
    email to a background job using Sidekiq and the delay method it
    provides. All this will be initialised in a after_create callback on
    the users model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, email: true, presence: true, uniqueness: {allow_blank: true}
+
+  after_create :send_welcome_email
+
+  def send_welcome_email
+    AdminMailer.delay.new_user(self)
+  end
 end
